32fa31d3079e34016f068e5e27ff562ce7659031
cuiruiqi <crqhit@163.com>
2019-05-31 14:33:04 +0800

    Update to 3.5

--- Chapter3/Answers.cpp
+++ Chapter3/Answers.cpp
@@ -39,25 +39,278 @@ void PrintLots(List l, List p)
 
 //3.3
 //通过只调整指针（而不是数据）来交换两个相邻的元素
-//单链表、双链表
-void ChangeElement(List l)
+//单链表:orig1为相邻两个元素的前一个元素
+int ChangeElement(List &l, PtrNode orig1)
 {
+	if (l == NULL || orig1->next == NULL)
+		return -1;
+	else
+	{	
+		PtrNode pre1 = NULL;
+		PtrNode temp = l;
+		
+		if (orig1 == l)
+		{
+			PtrNode orig2 = orig1->next;
+			orig1->next = orig2->next;
+			orig2->next = orig1;
+			l = orig2;
+		}
+		else
+		{
+			while (temp != NULL)
+			{
+				if (temp->next == orig1)
+				{
+					pre1 = temp;
+					break;
+				}
+				else
+					temp = temp->next;
+			}
 
+			PtrNode orig2 = orig1->next;
+			orig1->next = orig2->next;
+			orig2->next = orig1;
+			pre1->next = orig2;
+		}
+		
+		return 0;
+	}
 }
 
-void ChangeElement()
+void TestChangeElementList()
 {
+	printf("Test Change Element in List.\n");
+	List l = CreateList(4);
+	printf("Before change element. \n");
 
+	PrintList(l);
+	
+	printf("===After change element.===\n");
+	PtrNode p = l->next->next;
+	ChangeElement(l, p);
+	PrintList(l);
 }
+
+//双链表:orig1为相邻两个元素的前一个元素
+int ChangeElement(DList &l, PtrDNode orig1)
+{
+	if (l == NULL || orig1->next == NULL)
+		return -1;
+	else
+	{
+		//orig1为起始元素
+		if (l == orig1)
+		{
+			PtrDNode orig2 = orig1->next;
+			orig1->next = orig2->next;
+			orig1->last = orig2;
+
+			orig2->last = NULL;
+			orig2->next = orig1;
+			
+			l = orig2;
+		}
+		//orig1不为起始元素
+		else
+		{
+			PtrDNode orig2 = orig1->next;
+			PtrDNode pre1 = orig1->last;
+
+			orig1->next = orig2->next;
+			orig1->last = orig2;
+
+			pre1->next = orig2;
+			orig2->last = pre1;
+			orig2->next = orig1;
+		}
+
+		return 0;
+	}
+}
+
+void TestChangeElementDList()
+{
+	printf("Test Change Element in DList.\n");
+	DList l = CreateDList(4);
+	printf("Before change element. \n");
+	PrintDList(l);
+	printf("===After change element.===\n");
+	PtrDNode p = l->next;
+	ChangeElement(l, p);
+	PrintDList(l);
+}
+
+//3.4 给定两个已排序的表L1和L2，只使用基本的表操作编写计算L1&&L2的过程
+//假设L1与L2均按照升序排列
+List Intersection(List l1, List l2)
+{
+	List l = (List)malloc(sizeof(struct Node));
+	PtrNode p = l;
+	while (l1 != NULL && l2 != NULL)
+	{
+		if (l1->element == l2->element)
+		{
+			List temp = (List)malloc(sizeof(struct Node));
+			temp->element = l1->element;
+			p->next = temp;
+			temp->next = NULL;
+			p = p->next;
+
+			l1 = l1->next;
+			l2 = l2->next;
+		}
+		else if (l1->element > l2->element)
+		{
+			l2 = l2->next;
+		}
+		else
+		{
+			l1 = l1->next;
+		}
+	}
+
+	List result = l->next;
+	l->next = NULL;
+	free(l);
+	return result;
+}
+
+void CreateOrderList(List &list1, List &list2)
+{
+	List l1 = (List)malloc(sizeof(struct Node));
+	PtrNode p = l1;
+	for (size_t i = 0; i < 10; i++)
+	{
+		List temp = (List)malloc(sizeof(struct Node));
+		temp->element = i;
+		temp->next = NULL;
+		p->next = temp;
+		p = p->next;
+	}
+	list1 = l1->next;
+
+	List l2 = (List)malloc(sizeof(struct Node));
+	p = l2;
+	List l2_e1 = (List)malloc(sizeof(struct Node));
+	l2_e1->element = 3;
+	p->next = l2_e1;
+	p = p->next;
+
+	List l2_e2 = (List)malloc(sizeof(struct Node));
+	l2_e2->element = 4;
+	p->next = l2_e2;
+	p = p->next;
+
+	List l2_e3 = (List)malloc(sizeof(struct Node));
+	l2_e3->element = 11;
+	p->next = l2_e3;
+	p = p->next;
+	l2_e3->next = NULL;
+
+	list2 = l2->next;
+
+	printf("========print List1========.\n");
+	PrintList(list1);
+	printf("========print List2========.\n");
+	PrintList(list2);
+}
+
+void Test3_4()
+{
+	List list1, list2;
+	CreateOrderList(list1, list2);
+
+	List result = Intersection(list1, list2);
+	printf("========Intersection List========.\n");
+	PrintList(result);
+}
+
+//3.5 给定两个已排序的表L1和L2，只使用基本的表操作编写计算L1||L2的过程
+//假设L1与L2均按照升序排列
+
+void InsertSmaller(PtrNode &p, List &l1, List &l2)
+{
+	List temp = (List)malloc(sizeof(struct Node));
+	
+	if (l1->element == l2->element)
+	{
+		temp->element = l1->element;
+		l1 = l1->next;
+		l2 = l2->next;
+	}
+	else if (l1->element > l2->element)
+	{
+		temp->element = l2->element;
+		l2 = l2->next;
+	}
+	else
+	{
+		temp->element = l1->element;
+		l1 = l1->next;
+	}
+
+	temp->next = NULL;
+	p->next = temp;
+	p = p->next;
+}
+
+List Union(List l1, List l2)
+{
+	List l = (List)malloc(sizeof(struct Node));
+	PtrNode p = l;
+
+	while (l1 != NULL && l2 != NULL)
+	{
+		InsertSmaller(p, l1, l2);
+	}
+	if (l1 == NULL)
+	{
+		while (l2 != NULL)
+		{
+			List temp = (List)malloc(sizeof(struct Node));
+			temp->element = l2->element;
+			l2 = l2->next;
+			temp->next = NULL;
+			p->next = temp;
+			p = p->next;
+		}
+	}
+	else if (l2 == NULL)
+	{
+		while (l1 != NULL)
+		{
+			List temp = (List)malloc(sizeof(struct Node));
+			temp->element = l1->element;
+			l1 = l1->next;
+			temp->next = NULL;
+			p->next = temp;
+			p = p->next;
+		}
+	}
+
+	List result = l->next;
+	l->next = NULL;
+	free(l);
+	return result;
+}
+
+void Test3_5()
+{
+	List list1, list2;
+	CreateOrderList(list1, list2);
+
+	List result = Union(list1, list2);
+	printf("========Intersection List========.\n");
+	PrintList(result);
+}
+
+//3.6 
 
 int main()
 {
-    printf("start running.\n");
-    List l = CreateList(10);
-    List p = CreateList(2);
-    PrintList(l);
-    PrintList(p);
-    PrintLots(l, p);
+	Test3_5();
 
     return 0;
 }
--- Chapter3/Base.h
+++ Chapter3/Base.h
@@ -2,16 +2,26 @@
 #define BASE_H_
 
 #include <cstdlib>
+#include <cstdio>
 
 typedef int Element;
 typedef struct Node *PtrNode;
 typedef PtrNode List;
+typedef struct DoublyNode *PtrDNode;
+typedef PtrDNode DList;
 
 struct Node
 {
     Element element;
     Node *next;
 }typedef Node;
+
+struct DoublyNode
+{
+	Element element;
+	DoublyNode *last;
+	DoublyNode *next;
+}typedef DoublyNode;
 
 List CreateList(int len)
 {
@@ -27,9 +37,46 @@ List CreateList(int len)
     }
 
     List result = l->next;
-    l->next = NULL;
+    //l->next = NULL;
     free(l);
     return result;
+}
+
+DList CreateDList(int len)
+{
+	DList l = (DList)malloc(sizeof(struct DoublyNode));
+	PtrDNode p = l;
+	while (len-- > 0)
+	{
+		DList tmp = (DList)malloc(sizeof(struct DoublyNode));
+		tmp->element = rand() % 100;
+		tmp->last = p;
+		tmp->next = NULL;
+		p->next = tmp;
+		p = p->next;
+	}
+
+	DList result = l->next;
+	l->next = NULL;
+	free(l);
+	result->last = NULL;
+
+	return result;
+}
+
+void PrintDList(DList l)
+{
+	printf("Print Doubly list.\n");
+	if (l == NULL)
+		return;
+	else
+	{
+		while (l != NULL)
+		{
+			printf("element is: %d\n", l->element);
+			l = l->next;
+		}
+	}
 }
 
 #endif
